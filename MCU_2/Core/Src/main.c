0459bedd437cf29c2fff0149b377d739051b0588
LamTS72 <sonlam7220@gmail.com>
2023-09-19 16:39:05 +0700

    L2_910

--- MCU_2/Core/Src/main.c
+++ MCU_2/Core/Src/main.c
@@ -292,48 +292,16 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, PA1_Pin|PA2_Pin|PA3_Pin|PA4_Pin
-                          |PA5_Pin|PA6_Pin|PA7_Pin|PA8_Pin
-                          |PA9_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, PA1_Pin|PA2_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, PB0_Pin|PB1_Pin|PB2_Pin|PB10_Pin
-                          |PB11_Pin|PB12_Pin|PB13_Pin|PB14_Pin
-                          |PB15_Pin|PB3_Pin|PB4_Pin|PB5_Pin
-                          |PB6_Pin|PB8_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pins : PA1_Pin PA2_Pin PA3_Pin PA4_Pin
-                           PA5_Pin PA6_Pin PA7_Pin PA8_Pin
-                           PA9_Pin */
-  GPIO_InitStruct.Pin = PA1_Pin|PA2_Pin|PA3_Pin|PA4_Pin
-                          |PA5_Pin|PA6_Pin|PA7_Pin|PA8_Pin
-                          |PA9_Pin;
+  /*Configure GPIO pins : PA1_Pin PA2_Pin */
+  GPIO_InitStruct.Pin = PA1_Pin|PA2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PB0_Pin PB1_Pin PB2_Pin PB10_Pin
-                           PB11_Pin PB12_Pin PB13_Pin PB14_Pin
-                           PB15_Pin PB3_Pin PB4_Pin PB5_Pin
-                           PB6_Pin PB8_Pin */
-  GPIO_InitStruct.Pin = PB0_Pin|PB1_Pin|PB2_Pin|PB10_Pin
-                          |PB11_Pin|PB12_Pin|PB13_Pin|PB14_Pin
-                          |PB15_Pin|PB3_Pin|PB4_Pin|PB5_Pin
-                          |PB6_Pin|PB8_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PB9_Pin */
-  GPIO_InitStruct.Pin = PB9_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(PB9_GPIO_Port, &GPIO_InitStruct);
 
 }
 
